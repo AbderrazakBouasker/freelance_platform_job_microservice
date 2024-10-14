3f922b26f399795b3acb99a0988b6f7f53325f25
Jose Mokeni <jmmokeni@gmail.com>
2024-10-14 19:35:00 +0100

    updated hello controller

--- src/main/java/com/microservice/job/controller/Hello.java
+++ src/main/java/com/microservice/job/controller/Hello.java
@@ -1,4 +1,4 @@
-package controller;
+package com.microservice.job.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
