46e74ed1c65f38f0e47be725b7640062ad6582a8
Jose Mokeni <jmmokeni@gmail.com>
2024-11-16 20:49:51 +0100

    config from server ok

--- src/main/java/com/microservice/job/JobApplication.java
+++ src/main/java/com/microservice/job/JobApplication.java
@@ -2,8 +2,10 @@ package com.microservice.job;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class JobApplication {
 
 	public static void main(String[] args) {
